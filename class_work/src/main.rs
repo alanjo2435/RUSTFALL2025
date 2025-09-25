66288af474028561f66483668d8a7587896fcccb
alanjo2435 <alanjogarcia2435@gmail.com>
2025-09-25 03:24:01 +0000

    Practice Problems Complete

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,12 +1,32 @@
-
-fn append_region(word: &mut String) {
-    word.push_str("RGV");
+fn concat_strings(s1: &String, s2: &String) -> String {
+    let mut result = String::new();
+    result.push_str(s1);
+    result.push_str(s2);
+    result
 }
 
-fn main(){
-    let mut x = "UT".to_string();
-    append_region(&mut x);
+fn clone_and_modify(s: &String) -> String {
+    let mut cloned = s.clone();
+    cloned.push_str("Garcia");
+    cloned
+}
 
+fn sum(total: &mut i32, low: i32, high: i32) {
+    for i in low..=high {
+        *total += i;
+    }
+}
 
-    println!("{}", x);
+fn main() {
+    let a = String::from("Alan ");
+    let b = String::from("Garcia");
+    println!("{}", concat_strings(&a, &b));
+
+    let c = String::from("Alan ");
+    println!("{}", clone_and_modify(&c));
+    println!("{}", c);
+
+    let mut total = 1;
+    sum(&mut total, 1, 50);
+    println!("{}", total);
 }
